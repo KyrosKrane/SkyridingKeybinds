ece0837da6516aa70cb9e335f11c25ecbe54d96c
KyrosKrane <kyros@kyros.info>
2023-10-07 16:20:21 -0500

    Implement for dracthyr gliding

--- DRButtonFixer.lua
+++ DRButtonFixer.lua
@@ -9,43 +9,72 @@ local addonName, DRBF = ...
 
 
 -- "If you get on a dragonriding mount, override the keybind to this spell; and when you dismount, remove the override."
+-- "If you are a dracthyr and cast soar, override the keybind to this spell; and when you land, remove the override."
 
 -- random non-UI frame to hold our keybindings and events
 local DRBFFrame = CreateFrame("Frame")
 local Events = {}
 
 -- track our state
-local BindingSet = false
+local BindingSet = "none" -- valid values are none, dragonriding, and dracthyr
+local DracthyrGliding = false
 local Timer
 
 
 -- Settings to enable debug output
--- Note that this can be changed
+-- Note that this can be changed by a command line switch
 local DRBFDebugMode = false
 
 local function DebugPrint(...)
-    if DRBFDebugMode then print(...) end
+    if DRBFDebugMode then print("DRBF: ", ...) end
 end
+
 
 -- Toggles for the binding
 
+local function RemoveBinding()
+	DebugPrint("In RemoveBinding()")
+    if "none" == BindingSet then return end
+
+	ClearOverrideBindings(DRBFFrame)
+	BindingSet = "none"
+	DebugPrint("Binding removed")
+end -- RemoveBinding()
+
+
 local function SetBinding()
 	DebugPrint("In SetBinding()")
-	if BindingSet then return end
+
+	-- I'm not sure it's ever possible to get into a situation where you switch from dragonriding to Dracthyr gliding (or the other way) without interim events.
+	-- But just in case, handle this swap scenario.
+    if DracthyrGliding then
+		if "dracthyr" == BindingSet then
+			return
+		elseif "dragonriding" == BindingSet then
+			RemoveBinding()
+		end
+    else
+        if "dragonriding" == BindingSet then
+            return
+		elseif "dracthyr" == BindingSet then
+			RemoveBinding()
+        end
+	end
+
+	-- Set the right bindings
+	if DracthyrGliding then
+		SetOverrideBinding(DRBFFrame, false, "BUTTON5", "SPELL Surge Forward(Racial)")
+		SetOverrideBinding(DRBFFrame, false, "BUTTON4", "SPELL Skyward Ascent(Racial)")
+		BindingSet = "dracthyr"
+		DebugPrint("Dracthyr Binding set")
+    else
 	SetOverrideBinding(DRBFFrame, false, "BUTTON5", "SPELL Surge Forward")
 	SetOverrideBinding(DRBFFrame, false, "BUTTON4", "SPELL Skyward Ascent")
 	SetOverrideBinding(DRBFFrame, false, "BUTTON3", "SPELL Whirling Surge")
-    BindingSet = true
-	DebugPrint("Binding set")
+		BindingSet = "dragonriding"
+		DebugPrint("Dragonriding Binding set")
 end
-
-local function RemoveBinding()
-	DebugPrint("In RemoveBinding()")
-	if not BindingSet then return end
-	ClearOverrideBindings(DRBFFrame)
-	BindingSet = false
-	DebugPrint("Binding removed")
-end
+end -- SetBinding()
 
 
 -- This function holds the logic for when to toggle the bindings
@@ -54,12 +83,13 @@ local function CheckForStatusChange(IsCombatStarting)
 
     local isGliding, canGlide, forwardSpeed = C_PlayerInfo.GetGlidingInfo()
 	--DebugPrint("In Check, Gliding is ", isGliding, ", BindingSet is ", BindingSet)
-	if isGliding and not BindingSet then
+	if isGliding and "none" == BindingSet then
 		SetBinding()
-	elseif not isGliding and BindingSet then
+	elseif not isGliding and "none" ~= BindingSet then
 		RemoveBinding()
 	end
 end
+
 
 -- Functions to start and end the timer
 
@@ -90,6 +120,31 @@ end -- Events:PLAYER_REGEN_DISABLED()
 function Events:PLAYER_REGEN_ENABLED()
 	StartTimer()
 end -- Events:PLAYER_REGEN_ENABLED()
+
+-- This event fires when the game thinks we can glide, or when we stop being able to.
+-- Note that the event is NOT reliable when state is true; it fires in many cases where the player can't actually glide.
+function Events:PLAYER_CAN_GLIDE_CHANGED(NewGlideState)
+    --DebugPrint("PLAYER_CAN_GLIDE_CHANGED is now ", NewGlideState)
+
+    if DracthyrGliding and not NewGlideState then
+		DracthyrGliding = false
+		DebugPrint("Dracthyr Soar has ended")
+	end
+end -- Events:PLAYER_CAN_GLIDE_CHANGED()
+
+-- This event fires when a spell is cast successfully.
+-- We only care about one spell, so exit early if it's not our spell.
+local DracthyrSoarSpellID = 369536
+function Events:UNIT_SPELLCAST_SUCCEEDED(unitTarget, castGUID, spellID)
+    if "player" ~= unitTarget or DracthyrSoarSpellID ~= spellID then return end
+
+    DracthyrGliding = true
+	DebugPrint("Dracthyr Soar cast successfully")
+end -- Events:UNIT_SPELLCAST_SUCCEEDED()
+
+
+
+
 
 -- -- This event fires when we mount or dismount.
 -- function Events:PLAYER_MOUNT_DISPLAY_CHANGED()
